40e703073ab70caf61657110eff6ee30b02370ea
Facundo Romano <facundo-romano@hotmail.com>
2022-11-08 10:57:07 -0300

    Practica_Finalizada

--- src/practica_4/components/LogicGate.js
+++ src/practica_4/components/LogicGate.js
@@ -12,23 +12,28 @@ const LogicGate = ({ resultinput1, resultinput2, takeResultHandler }) => {
     const calculate = () => {
       let input;
       switch (selected) {
-        case 0:
-          input = resultinput1 || resultinput2;
+        case "0":
+          console.log("OR")
+          input = parseInt( resultinput1) || parseInt(resultinput2);
           setResult(input)
           break;
-        case 1:
+        case "1":
+          console.log("AND")
           input = resultinput1 && resultinput2;
           setResult(input)
           break;
-        case 2:
+        case "2":
+          console.log("NAND")
           input = !(resultinput1 && resultinput2) ? 1 : 0;
           setResult(input)
           break;
-        case 3:
+        case "3":
+          console.log("NOR")
           input = !(resultinput1 || resultinput2) ? 1 : 0;
           setResult(input)
           break;
-        case 4:
+        case "4":
+          console.log("XOR")
           input = (resultinput1 && !resultinput2) || (!resultinput1 && resultinput2) ? 1 : 0;
           setResult(input)
           break;
@@ -46,11 +51,11 @@ const LogicGate = ({ resultinput1, resultinput2, takeResultHandler }) => {
     <div>
       <label>Compuerta lógica:</label>
       <select onChange={selectHandler}>
-        <option value={0}>OR</option>
-        <option value={1}>AND</option>
-        <option value={2}>NAND</option>
-        <option value={3}>NOR</option>
-        <option value={4}>XOR</option>
+        <option value="0">OR</option>
+        <option value="1">AND</option>
+        <option value="2">NAND</option>
+        <option value="3">NOR</option>
+        <option value="4">XOR</option>
       </select>
     </div>
     <h3>Resultado: {result}</h3>
